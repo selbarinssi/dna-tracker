2cec7e87a039fa1e99745dde2ea741e797c4b8fb
selbarinssi <elbarinssis@gmail.com>
2026-09-25 00:23:13 +0100

    Update layout.tsx

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,6 +1,5 @@
 // app/(dashboard)/layout.tsx
 
-import { Sidebar } from "@/components/layout/Sidebar";
 import { Header } from "@/components/layout/Header";
 
 export default function DashboardLayout({
@@ -9,13 +8,9 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex h-screen bg-ivory overflow-hidden">
-      <Sidebar />
-
-      <div className="flex-1 flex flex-col min-w-0">
-        <Header />
-        <main className="flex-1 overflow-auto p-6">{children}</main>
-      </div>
+    <div className="flex flex-col h-screen bg-ivory overflow-hidden">
+      <Header />
+      <main className="flex-1 overflow-auto p-6">{children}</main>
     </div>
   );
 }
